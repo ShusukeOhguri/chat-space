cc1d25965297675682458b4152125e04c6f121e7
Create unity project <shusuke_ohguri@OhguriMacBook-Air.local>
2017-06-13 22:14:49 +0900

    ユーザー情報更新機能追加

--- chat-space/app/controllers/users_controller.rb
+++ chat-space/app/controllers/users_controller.rb
@@ -1,2 +1,23 @@
 class UsersController < ApplicationController
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    user = User.find(params[:id])
+      if user.id == current_user.id
+        if params.require(:user)[:name].present?
+          if params.require(:user)[:email].present?
+            user.update(user_params)
+          end
+        end
+      end
+    redirect_to controller: :messages, action: :index
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email)
+    end
 end
