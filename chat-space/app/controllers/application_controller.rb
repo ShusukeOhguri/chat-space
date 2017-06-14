6738293666550913087b9a06fe4b808b34c43d71
Create unity project <shusuke_ohguri@OhguriMacBook-Air.local>
2017-06-14 22:08:58 +0900

    edit Flash message

--- chat-space/app/controllers/application_controller.rb
+++ chat-space/app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  add_flash_types :success, :info, :warning, :danger
   protect_from_forgery with: :exception
 end
