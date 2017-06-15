06a3899b2ce0ff3339311531d64aa19c345bf6ed
Create unity project <shusuke_ohguri@OhguriMacBook-Air.local>
2017-06-15 20:55:16 +0900

    指摘箇所の修正

--- chat-space/app/controllers/users_controller.rb
+++ chat-space/app/controllers/users_controller.rb
@@ -1,22 +1,17 @@
 class UsersController < ApplicationController
   def index
-    redirect_to new_user_session_path
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
-    if @user.id == current_user.id
-      if @user.update(user_params)
-        flash[:notice] = "更新に成功しました"
-        redirect_to root_path
-      else
-        flash[:alert] =  "更新に失敗しました"
-        render "edit"
-      end
+    if current_user.update(user_params)
+      flash[:notice] = "更新に成功しました"
+      redirect_to root_path
+    else
+      flash[:alert] =  "更新に失敗しました"
+      render "edit"
     end
   end
 
