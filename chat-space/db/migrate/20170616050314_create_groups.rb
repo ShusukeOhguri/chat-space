8857dc93e4be7eaa09cb99e6d2bff7ddb34d3660
Create unity project <shusuke_ohguri@OhguriMacBook-Air.local>
2017-06-16 14:20:02 +0900

    Edit migration file

---
+++ chat-space/db/migrate/20170616050314_create_groups.rb
@@ -0,0 +1,8 @@
+class CreateGroups < ActiveRecord::Migration[5.0]
+  def change
+    create_table :groups do |t|
+      t.integer :group_id, index: true, null: false, foreign_key: true
+      t.integer :user_id, index: true, null: false, foreign_key: true
+    end
+  end
+end
