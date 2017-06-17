ae53683fb8d703923e75293e80a52e7960b065b7
Create unity project <shusuke_ohguri@OhguriMacBook-Air.local>
2017-06-17 15:41:36 +0900

    グループ編集機能追加

--- chat-space/app/controllers/groups_controller.rb
+++ chat-space/app/controllers/groups_controller.rb
@@ -2,22 +2,22 @@ class GroupsController < ApplicationController
   def index
   end
 
-  def edit
-    @group = Group.find(params[:id])
-  end
-
-  def update
-    @group = Group.find(params[:id])
-    if @group.update(update_params)
-      redirect_to root_path, notice: "グループの情報の更新に成功しました"
-    else
-      flash[:alert] =  "グループ情報の更新に失敗しました"
-      render "edit"
+    def edit
+      @group = Group.find(params[:id])
     end
-  end
+  
+    def update
+      @group = Group.find(params[:id])
+      if @group.update(update_params)
+        redirect_to root_path, notice: "グループの情報の更新に成功しました"
+      else
+        flash[:alert] =  "グループ情報の更新に失敗しました"
+        render "edit"
+      end
+    end
 
   private
   def update_params
-    params.require(:user).permit(:group_id, :user_id)
+    params.require(:group).permit(:name)
   end
 end
