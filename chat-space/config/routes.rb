cb9ecff15cf9750a1572a198b28417291778cade
Create unity project <shusuke_ohguri@OhguriMacBook-Air.local>
2017-06-19 21:07:50 +0900

    Edit routes

--- chat-space/config/routes.rb
+++ chat-space/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users
-  resources :groups, only: [:index, :edit, :update]
+  resources :groups, only: [:new, :create, :edit, :update]
   root  'messages#index'
 end
