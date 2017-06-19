d5ce524896f14293739c8063283b7456b52ef718
Create unity project <shusuke_ohguri@OhguriMacBook-Air.local>
2017-06-19 21:08:15 +0900

    グループ作成機能記述

--- chat-space/app/controllers/groups_controller.rb
+++ chat-space/app/controllers/groups_controller.rb
@@ -2,21 +2,40 @@ class GroupsController < ApplicationController
   def index
   end
 
-    def edit
-      @group = Group.find(params[:id])
+  def new
+    @group = Group.new
+  end
+
+  def create
+    @group = Group.new(create_params)
+    binding.pry
+    if @group.save
+      redirect_to root_path, notice: "グループの情報の作成に成功しました"
+    else
+      flash[:alert] =  "グループ情報の作成に失敗しました"
+      render "new"
     end
-  
-    def update
-      @group = Group.find(params[:id])
-      if @group.update(update_params)
-        redirect_to root_path, notice: "グループの情報の更新に成功しました"
-      else
-        flash[:alert] =  "グループ情報の更新に失敗しました"
-        render "edit"
-      end
+  end
+
+  def edit
+    @group = Group.find(params[:id])
+  end
+
+  def update
+    @group = Group.find(params[:id])
+    if @group.update(update_params)
+      redirect_to root_path, notice: "グループの情報の更新に成功しました"
+    else
+      flash[:alert] =  "グループ情報の更新に失敗しました"
+      render "edit"
     end
+  end
 
   private
+  def create_params
+    params.require(:group).permit(:name, user_ids: [])
+  end
+
   def update_params
     params.require(:group).permit(:name)
   end
