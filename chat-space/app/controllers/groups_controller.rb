8c1a549a42eb621cdf89cf791c0820c97ffccc0f
Create unity project <shusuke_ohguri@OhguriMacBook-Air.local>
2017-06-20 18:01:17 +0900

    before_action :get_group, only: [:edit, :update]と記述し get_groupメソッドを作成

--- chat-space/app/controllers/groups_controller.rb
+++ chat-space/app/controllers/groups_controller.rb
@@ -1,5 +1,5 @@
 class GroupsController < ApplicationController
-  before_action :edit, only: :update
+  before_action :get_group, only: [:edit, :update]
 
   def index
   end
@@ -17,20 +17,23 @@ class GroupsController < ApplicationController
     end
   end
 
-  def edit  
-    @group = Group.find(params[:id])  
-  end 
-  
-  def update  
-    if @group.update(create_params) 
-      redirect_to root_path, notice: "グループの情報の更新に成功しました"  
-    else  
-      render "edit" 
-    end 
-  end 
-  
-  private 
-    def create_params 
-      params.require(:group).permit(:name, user_ids: [])  
-    end 
+  def edit
+  end
+
+  def update
+    if @group.update(create_params)
+      redirect_to root_path, notice: "グループの情報の更新に成功しました"
+    else
+      render "edit"
+    end
+  end
+
+  private
+    def get_group
+      @group = Group.find(params[:id])
+    end
+
+    def create_params
+      params.require(:group).permit(:name, user_ids: [])
+    end
 end
