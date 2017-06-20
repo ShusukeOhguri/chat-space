cdac901ec88812974eadea25566d7bddc2642cb1
Create unity project <shusuke_ohguri@OhguriMacBook-Air.local>
2017-06-20 14:38:15 +0900

    グループ情報更新機能の記述
    前回のブランチでほぼ更新機能を実装してしまったので、Gitで目立つようインデントを追加

--- chat-space/app/controllers/groups_controller.rb
+++ chat-space/app/controllers/groups_controller.rb
@@ -18,21 +18,21 @@ class GroupsController < ApplicationController
     end
   end
 
-  def edit
-    @group = Group.find(params[:id])
-  end
-
-  def update
-    if @group.update(create_params)
-      redirect_to root_path, notice: "グループの情報の更新に成功しました"
-    else
-      flash[:alert] =  "グループ情報の更新に失敗しました"
-      render "edit"
-    end
-  end
-
-  private
-  def create_params
-    params.require(:group).permit(:name, user_ids: [])
-  end
+  def edit  
+    @group = Group.find(params[:id])  
+  end 
+  
+  def update  
+    if @group.update(create_params) 
+      redirect_to root_path, notice: "グループの情報の更新に成功しました"  
+    else  
+      flash[:alert] =  "グループ情報の更新に失敗しました" 
+      render "edit" 
+    end 
+  end 
+  
+  private 
+  def create_params 
+    params.require(:group).permit(:name, user_ids: [])  
+  end 
 end
