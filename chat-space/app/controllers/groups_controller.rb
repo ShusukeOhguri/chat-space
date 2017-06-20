40ef4fc2678a1457615ff693baf095b7acb3f164
Create unity project <shusuke_ohguri@OhguriMacBook-Air.local>
2017-06-20 15:19:35 +0900

    privateメソッドの後を改行

--- chat-space/app/controllers/groups_controller.rb
+++ chat-space/app/controllers/groups_controller.rb
@@ -32,7 +32,7 @@ class GroupsController < ApplicationController
   end 
   
   private 
-  def create_params 
-    params.require(:group).permit(:name, user_ids: [])  
-  end 
+    def create_params 
+      params.require(:group).permit(:name, user_ids: [])  
+    end 
 end
