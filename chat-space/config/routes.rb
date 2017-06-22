2bcaa995a64ed2c6469f0d4e85d7777093a3fa8c
Create unity project <shusuke_ohguri@OhguriMacBook-Air.local>
2017-06-22 15:00:23 +0900

    Show group information

--- chat-space/config/routes.rb
+++ chat-space/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users
-  resources :groups, only: [:new, :create, :edit, :update]
+  resources :groups, only: [:new, :create, :edit, :show, :update]
   resources :messages
   root  'groups#index'
 end
