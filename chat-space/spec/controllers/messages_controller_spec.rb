7328bc5d287052003b406e0ac7532a7be5334825
Create unity project <shusuke_ohguri@OhguriMacBook-Air.local>
2017-07-06 16:25:39 +0900

    Add  'messages#index'  'messages#create'

--- chat-space/spec/controllers/messages_controller_spec.rb
+++ chat-space/spec/controllers/messages_controller_spec.rb
@@ -15,56 +15,60 @@ describe MessagesController, type: :controller do
       login_user user
     end
 
-    it "アクション内で定義しているインスタンス変数があるか" do
-      group = user.groups.first
-      get :index, params:{group_id: group.id}
-
-      fake_groups = user.groups
-      expect(assigns(:groups)).to eq fake_groups
-
-      group_members_F = fake_groups.find(group.id).users
-      expect(assigns(:group_members)).to eq group_members_F
-
-      group_messages_F = fake_groups.find(group.id).messages
-      expect(assigns(:group_messages)).to eq group_messages_F
-
-      expect(assigns(:message)).to be_an_instance_of Message
-    end
-
-    it "該当するビューが描画されているか" do
-      group = user.groups.first
-      get :index, params:{group_id: group.id}
-      expect(response).to render_template :index
-    end
-
-    describe '保存に成功した場合' do
-      it "メッセージの保存は行われたか" do
+    describe 'messages#index' do
+      it "アクション内で定義しているインスタンス変数があるか" do
         group = user.groups.first
-        post :create, params:{group_id: group.id, message: attributes_for(:message)}
-        expect(assigns(:message).save).to be_truthy 
-      end
+        get :index, params:{group_id: group.id}
   
-      it "メッセージの保存画面に遷移しているか" do
+        fake_groups = user.groups
+        expect(assigns(:groups)).to eq fake_groups
+  
+        group_members_F = fake_groups.find(group.id).users
+        expect(assigns(:group_members)).to eq group_members_F
+  
+        group_messages_F = fake_groups.find(group.id).messages
+        expect(assigns(:group_messages)).to eq group_messages_F
+  
+        expect(assigns(:message)).to be_an_instance_of Message
+      end
+
+      it "該当するビューが描画されているか" do
         group = user.groups.first
         get :index, params:{group_id: group.id}
         expect(response).to render_template :index
       end
     end
-
-    describe '保存に失敗した場合' do
-      it "メッセージの保存は行われなかったか" do
-        group = user.groups.first
-        new_message = attributes_for(:message)
-        new_message[:text] = nil
-        new_message[:image] = nil
-        post :create, params:{group_id: group.id, message: new_message}
-        expect(assigns(:message).save).to be_falsey 
+  
+    describe 'messages#create' do
+      describe '保存に成功した場合' do
+        it "メッセージの保存は行われたか" do
+          group = user.groups.first
+          post :create, params:{group_id: group.id, message: attributes_for(:message)}
+          expect(assigns(:message).save).to be_truthy 
+        end
+    
+        it "メッセージの保存画面に遷移しているか" do
+          group = user.groups.first
+          get :index, params:{group_id: group.id}
+          expect(response).to render_template :index
+        end
       end
   
-      it "意図したビューが描画されているか" do
-        group = user.groups.first
-        get :index, params:{group_id: group.id}
-        expect(response).to render_template :index
+      describe '保存に失敗した場合' do
+        it "メッセージの保存は行われなかったか" do
+          group = user.groups.first
+          new_message = attributes_for(:message)
+          new_message[:text] = nil
+          new_message[:image] = nil
+          post :create, params:{group_id: group.id, message: new_message}
+          expect(assigns(:message).save).to be_falsey
+        end
+    
+        it "意図したビューが描画されているか" do
+          group = user.groups.first
+          get :index, params:{group_id: group.id}
+          expect(response).to render_template :index
+        end
       end
     end
   end
