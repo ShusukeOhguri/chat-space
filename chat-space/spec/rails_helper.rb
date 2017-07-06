5d4b42c1aa44d4f7bbd084c829401ebd92c7fc25
Create unity project <shusuke_ohguri@OhguriMacBook-Air.local>
2017-07-06 14:36:49 +0900

    Edit rspec Configuration

--- chat-space/spec/rails_helper.rb
+++ chat-space/spec/rails_helper.rb
@@ -1,10 +1,11 @@
 require 'spec_helper'
 require 'devise'
+require 'rspec/rails'
 require File.expand_path("spec/support/controller_macros.rb")
 
 RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
-  config.include Devise::TestHelpers, type: :controller
+  config.include Devise::Test::ControllerHelpers, type: :controller
   config.include ControllerMacros, type: :controller
 end
 
--- chat-space/spec/spec_helper.rb
+++ chat-space/spec/spec_helper.rb
@@ -31,7 +31,7 @@ RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
-  # examples within a transaction, remove the following line or assign false
+  # examples within a transaction, remove the following line or assign falsez
   # instead of true.
   config.use_transactional_fixtures = true
 
@@ -44,8 +44,7 @@ RSpec.configure do |config|
   # order dependency and want to debug it, you can fix the order by providing
   # the seed, which is printed after each run.
   #     --seed 1234
-  config.order = "random"
-  # config.include FactoryGirl::Syntax::Methods
+  config.include FactoryGirl::Syntax::Methods
   # config.infer_spec_type_from_file_location!
 
   config.include Devise::Test::ControllerHelpers, type: :controller
