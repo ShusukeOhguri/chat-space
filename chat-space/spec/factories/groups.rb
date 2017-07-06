598cb6b8e2a60eb0293cd5f684ee8d974740549a
Create unity project <shusuke_ohguri@OhguriMacBook-Air.local>
2017-07-06 16:30:19 +0900

    Delete ID

--- chat-space/spec/factories/groups.rb
+++ chat-space/spec/factories/groups.rb
@@ -1,10 +1,5 @@
 FactoryGirl.define do
   factory :group do
-    id          "1"
     name        {Faker::Name.title}
-
-    after(:create) do |group|
-      group.messages << create_list(:message, 5)
-    end
   end
 end
--- chat-space/spec/factories/messages.rb
+++ chat-space/spec/factories/messages.rb
@@ -2,8 +2,8 @@ FactoryGirl.define do
   factory :message do
     text        {Faker::Pokemon.name}
     image       {File.open("#{Rails.root}/spec/images/1.jpg")}
-    user_id     "1"
-    group_id    "1"
+    user
+    group
     created_at  { Faker::Time.between(2.days.ago, Time.now, :all) }
     updated_at  { Faker::Time.between(2.days.ago, Time.now, :all) }
   end
